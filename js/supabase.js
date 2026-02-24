1ea4fb1d930ecf9ca0e135e923eb59597a23295d
Philipp Jüde <ph.juede@gmail.com>
2026-02-24 10:30:16 +0100

    updated structure

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+const supabaseUrl = "https://kstqhcaazuuxchqtnyfc.supabase.co";
+const supabaseKey = "sb_publishable_0C-Hj42NxQ1UCHMkadC-Pw_KWDg6o2r";
+
+window.supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+console.log("Supabase initialisiert");
